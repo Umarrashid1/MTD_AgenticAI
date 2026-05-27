53e176b4490f5e27848da4cca0ca6283b9ab662a
umarrashid <umarrashid@hotmail.dk>
2026-05-27 16:03:36 +0200

    minor

--- network/topology.py
+++ network/topology.py
@@ -29,7 +29,7 @@ def create_topology():
 
     info('*** Adding Docker containers (Internal Zone)\n')
     # Legitimate Client
-    c1 = net.addDocker('c1', ip='10.0.0.11/24', mac='00:00:00:00:00:AA', dimage="ubuntu:trusty")
+    c1 = net.addDocker('c1', ip='10.0.0.1/24', mac='00:00:00:00:00:AA', dimage="ubuntu:trusty")
     # Decoy?
     decoy = net.addDocker('decoy', ip='10.0.0.2/24', mac='00:00:00:00:00:DD', dimage="ubuntu:trusty")
     # Vulnerable Target
